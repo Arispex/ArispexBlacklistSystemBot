8033ca16a09c2de59edbefdb864e2159ed6c5982
Arispex <Arispex@outlook.com>
2023-08-01 22:04:16 +0800

    修复了菜单没有判断

--- ArispexBlacklistSystemBot/Plugins/Misc.cs
+++ ArispexBlacklistSystemBot/Plugins/Misc.cs
@@ -20,22 +20,28 @@ public class Misc: CqPostPlugin
     public override async Task OnPrivateMessageReceivedAsync(CqPrivateMessagePostContext context)
     {
         if (context.Session is not ICqActionSession actionSession) return;
-        
-        if (context.UserId == long.Parse(_configuration["OwnerId"]))
+
+        if (context.Message.Text.StartsWith("菜单"))
         {
-            await actionSession.SendPrivateMessageAsync(context.UserId, new CqMessage("授权群\n取消授权群\n授权用户\n取消授权用户\n添加云黑\n删除云黑\n云黑信息"));
+            if (context.UserId == long.Parse(_configuration["OwnerId"]))
+            {
+                await actionSession.SendPrivateMessageAsync(context.UserId, new CqMessage("授权群\n取消授权群\n授权用户\n取消授权用户"));
+                return;
+            }
+            await actionSession.SendPrivateMessageAsync(context.UserId, new CqMessage("该功能仅限主人可用。"));
             return;
         }
-        await actionSession.SendPrivateMessageAsync(context.UserId, new CqMessage("该功能仅限主人可用。"));
-        return;
     }
 
     public override async Task OnGroupMessageReceivedAsync(CqGroupMessagePostContext context)
     {
         if (context.Session is not ICqActionSession actionSession) return;
         if (!_applicationDbContext.AuthorizedGroups.Select(x => x.GroupId).ToList().Contains(context.GroupId.ToString())) return;
-        await actionSession.SendGroupMessageAsync(context.GroupId,
-            new CqMessage("授权群\n取消授权群\n授权用户\n取消授权用户\n添加云黑\n删除云黑\n云黑信息"));
-        return;
+        if (context.Message.Text.StartsWith("菜单"))
+        {
+            await actionSession.SendGroupMessageAsync(context.GroupId,
+                new CqMessage("添加云黑\n删除云黑\n云黑信息"));
+            return;
+        }
     }
 }
